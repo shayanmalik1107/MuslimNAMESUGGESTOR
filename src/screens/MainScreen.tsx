ab279ee9df2a2cf3fa0c4e78d09be2e1b5a5718a
shayanmalik1107 <shayanmalik1107@gmail.com>
2025-05-03 18:36:09 +0500

    log

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,4 +1,4 @@
-import React, {  useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import App from '../../App';
 import { constant } from '../utils/mesgs';
@@ -6,28 +6,29 @@ import { colors } from '../utils/colors';
 import { font } from '../asset/fonts';
 const MainScreen = () => {
 
+  console.log('sadsd');
   return (
     <View style={styling.backgound}>
-        <Text style={styling.Text1}>
+      <Text style={styling.Text1}>
         {constant.thumbnail}
-        </Text>
-        <Text style={styling.Text2}>
-       {constant.subthumbnail}
-        </Text>
-  </View>
+      </Text>
+      <Text style={styling.Text2}>
+        {constant.subthumbnail}
+      </Text>
+    </View>
 
   );
 };
 const styling = StyleSheet.create({
-	backgound: {
-		backgroundColor:colors.background, flex: 1, justifyContent: 'center', alignItems: 'center',
-	},
-	Text1: {
+  backgound: {
+    backgroundColor: colors.background, flex: 1, justifyContent: 'center', alignItems: 'center',
+  },
+  Text1: {
     color: colors.thumbText, fontSize: 50,
-    fontFamily:font.headingfontfamily,
-	},
-	Text2: {
-		color:colors.subthumb,fontSize:18,fontFamily :font.subheadingfontfamily,
-	},
+    fontFamily: font.headingfontfamily,
+  },
+  Text2: {
+    color: colors.subthumb, fontSize: 18, fontFamily: font.subheadingfontfamily,
+  },
 });
 export default MainScreen;
